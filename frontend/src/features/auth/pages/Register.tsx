23f22c28dbb66c6da7b805cfb12b174f6a7a2ade
omkardusane <omkardusane@gmail.com>
2025-08-06 07:29:47 +0200

    preparing register payload

--- frontend/src/features/auth/pages/Register.tsx
+++ frontend/src/features/auth/pages/Register.tsx
@@ -2,23 +2,34 @@
 import React from 'react';
 import { Link } from 'react-router'
 
+export interface RegisterForm {
+  email: string;
+  name: string;
+  role: 'patient' | 'doctor';
+}
+
 export default function Register() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email') as string;
+    const name = formData.get('name') as string;
+    const role = formData.get('role') as 'patient' | 'doctor';
+
+    const registerForm: RegisterForm = {
+      email,
+      name,
+      role,
+    };
+
+    console.log('ctarg:', e.currentTarget);
+    console.log('formData:', formData);
+    console.log('registerForm:', registerForm);
+  };
+
   return (
     <div>
-      <form
-        style={{ color: '#07446d' }}
-        onSubmit={(e) => {
-          e.preventDefault();
-          const formData = new FormData(e.currentTarget);
-          const email = formData.get('email') as string;
-          const name = formData.get('name') as string;
-          const role = formData.get('role') as string;
-
-          console.log('Email:', email);
-          console.log('Name:', name);
-
-        }}
-      >
+      <form onSubmit={handleSubmit}>
         <label>
           Email:
           <input className="block w-full px-5 py-3 text-base text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500" type="email" name="email" />
